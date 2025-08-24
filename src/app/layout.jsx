5e95fbba449f7378727f4bf846c0a82960ab94cf
chrxsjung <jungchristopher456@gmail.com>
2025-08-24 00:10:50 -0400

    speed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Chris Jung | Portfolio",
@@ -63,6 +64,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
