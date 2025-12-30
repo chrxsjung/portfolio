5a76e7e7113cf3e838f5baffc179f9aaebef9bd9
chrxsjung <jungchristopher456@gmail.com>
2025-12-30 13:55:36 -0500

    yo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Chris Jung | Portfolio",
